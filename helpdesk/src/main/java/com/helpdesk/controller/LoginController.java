6c702aed2fb81a3b0d02835b2d0280fee5513f02
FelixSanthanasekar <felixsanthanasekar@gmail.com>
2025-07-24 23:51:56 -0400

    login changes issue fixed

--- helpdesk/src/main/java/com/helpdesk/controller/LoginController.java
+++ helpdesk/src/main/java/com/helpdesk/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.helpdesk.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class LoginController {
     @GetMapping("/login")
-    @Operation(summary = "Login Details")
     public String login() {
         return "Hello Help Desk";
     }
